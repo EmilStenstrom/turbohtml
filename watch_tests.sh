42bfa339f41a712852abc181aa667f853dcb7de4
Emil Stenström <emil@emilstenstrom.se>
2025-11-23 22:35:52 +0100

    Refactor: remove unnecessary break statement in watch_tests.sh loop

--- watch_tests.sh
+++ watch_tests.sh
@@ -7,5 +7,5 @@ trap 'echo; echo "Interrupted, exiting."; exit 130' INT
 while sleep 0.1; do
   # When entr or the test command is interrupted, entr should exit non‑zero.
   # In that case, we break out of the loop.
-  find . -type f -name '*.py' -o -name '*.test' -o -name '*.dat' | entr -crd python run_tests.py -q --regressions || break
+  find . -type f -name '*.py' -o -name '*.test' -o -name '*.dat' | entr -crd python run_tests.py -q --regressions
 done
